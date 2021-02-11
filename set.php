2b04b8a30b5994ac1d53162123e0d343906da76b
jabsatan <jabsatan666@hotmail.com>
2021-02-11 10:19:03 +0700

    Create set.php

---
+++ set.php
@@ -0,0 +1,27 @@
+<html>
+<header></header>
+<body>
+
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://th.tradingview.com/symbols/SET-OR/" rel="noopener" target="_blank"><span class="blue-text">OR ราคา</span></a> โดย TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
+  {
+  "symbol": "SET:OR",
+  "width": 350,
+  "height": 220,
+  "locale": "th_TH",
+  "dateRange": "12M",
+  "colorTheme": "light",
+  "trendLineColor": "#37a6ef",
+  "underLineColor": "#E3F2FD",
+  "isTransparent": false,
+  "autosize": false,
+  "largeChartUrl": ""
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
+
+</body></html>
